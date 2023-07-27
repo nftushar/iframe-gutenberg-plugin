526f2d4b9496dff4a3a26ca4817cd998dafb6559
nftushar <nftushar5200@gmail.com>
2023-07-27 16:00:46 +0600

    General and style controll separated

---
+++ src/Iframe.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { maximizeIcon, minimizeIcon } from './utils/icons';
+
+const Iframe = ({ attributes, clientId }) => {
+    const { src, title, loading, isFullScreen } = attributes;
+
+    const [isNowFull, setIsNowFull] = useState(false);
+
+    const onFullScreen = () => {
+        const element = document.querySelector(`#ifmIframe-${clientId}`);
+
+        if (document.fullscreenElement) {
+            setIsNowFull(false);
+            document.exitFullscreen();
+        } else {
+            setIsNowFull(true);
+            element.requestFullscreen();
+        }
+    };
+
+    return <div className='ifmIframe'>
+        <iframe
+            title={title}
+            width='100%'
+            height='100%'
+            src={src}
+            loading={loading}
+            allowfullscreen={isFullScreen}
+        ></iframe>
+
+        {(isFullScreen && !src.includes('youtube.com/embed')) && (
+            <button onClick={onFullScreen} className="fullScreenBtn" dangerouslySetInnerHTML={{ __html: isNowFull ? minimizeIcon : maximizeIcon }} />
+        )}
+    </div>
+}
+export default Iframe;
